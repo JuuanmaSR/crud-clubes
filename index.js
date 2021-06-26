9d7e5c847be741d5e59b7db6cf914869e9eff360
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-06-26 15:09:18 -0300

    Creando rutas  de la app

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 
 const upload = multer({ dest: './uploads/images' });
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 const PUERTO = 8080;
 const app = express();
@@ -14,13 +15,45 @@ const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(`${__dirname}/public`)));
 app.use(express.static(`${__dirname}/uploads`));
 
+const equipos = [
+  { nombre: 'River' },
+  { nombre: 'Boca' },
+  { nombre: 'independiente' },
+];
+
+// Route
 app.get('/', (req, res) => {
   res.render('inicio', {
+    layout: 'home',
+    style: 'inicio.css',
+    equipos,
+
+  });
+});
+
+// Agregar
+app.get('/agregar', (req, res) => {
+  res.render('agregar', {
+    layout: 'home',
+    style: 'agregar.css',
+  });
+});
+
+app.post('/agregar', (req, res) => {
+  res.render('agregar', {
     layout: 'home',
   });
 });
 
+//
+app.get('/equipo/:id/ver', (req, res) => {
+  res.render('ver', {
+    layout: 'home',
+    style: 'ver.css',
+  });
+});
 app.listen(8080);
 console.log(`Escuchando en el puerto ${PUERTO}`);
