02fed94c012f6cdb5605df5e8a92b7294304ad39
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-07-07 14:02:08 -0300

    update-index
    agregando archivos estáticos  y estructurando como se guardan los datos de los equipos.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const express = require('express');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
-const upload = multer({ dest: './uploads/images' });
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, './uploads/images');
@@ -29,15 +28,20 @@ const app = express();
 const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
+// Settings
 app.set('view engine', 'handlebars');
 
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// Static files
+app.use(express.static(path.join(`${__dirname}/views`)));
+app.use(express.static(path.join(`${__dirname}/public`)));
+app.use(express.static(path.join(`${__dirname}/uploads`)));
 
 let equipos = require('./data/equipos.json');
 
-// Routing
+// Routes
 app.get('/', (req, res) => {
   res.render('inicio', {
     layout: 'index',
@@ -55,10 +59,9 @@ app.get('/agregar', (req, res) => {
   });
 });
 
-app.post('/agregar', (req, res) => {
-  console.log(req.body);
-  const { nombre, logo, pais } = req.body;
-  if (!nombre || !logo || !pais) {
+app.post('/agregar', upload.single('imagen'), (req, res) => {
+  const { nombre, pais } = req.body;
+  if (!nombre || !pais) {
     res.status(400).send('Faltan campos por completar');
     return;
   }
@@ -66,8 +69,8 @@ app.post('/agregar', (req, res) => {
     id: uuidv4(),
     area: { name: pais },
     name: nombre,
-    crestUrl: logo,
-    lastUpdate: new Date(),
+    crestUrl: `/images/${req.file.filename}`,
+    lastUpdate: Date(),
 
   };
   equipos.push(newEquipo);
