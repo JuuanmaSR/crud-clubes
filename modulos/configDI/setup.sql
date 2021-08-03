b03d2d9310c9760a67ed696e12a2e6e2a2fb1dba
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-08-03 00:54:32 -0300

    delete-querys
    Eliminando querys de prueba para mejorar la prolijidad del código de setup.

--- modulos/configDI/setup.sql
+++ modulos/configDI/setup.sql
@@ -9,10 +9,3 @@ CREATE TABLE  equipos(
     created_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
     updated_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL
 );
-
-INSERT INTO  equipos(area_name, name, short_name,crest_url,address ) VALUES ('Argentina','River', 'CARP','./images/CA_river_plate_logo.svg.png','Av.Siempreviva');
-SELECT * FROM equipos;
-DELETE  FROM equipos;
-DELETE FROM equipos WHERE id = 1
-DELETE FROM sqlite_sequence WHERE NAME='equipos'
-UPDATE equipos SET crest_url = 'images/CA_river_plate_logo.svg.png'
