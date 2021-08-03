8225bc21cb85a835a2c7e13f11d851fa71ca0f0f
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-08-03 00:51:30 -0300

    update-clubrepository
    Se modifican las dependencias por el uso de SQLite como base de datos y se pasa utilizar en cada método que brinda esta entidad para mejorar el manejo de datos.

--- modulos/club/repository/clubRepository.js
+++ modulos/club/repository/clubRepository.js
@@ -1,63 +1,113 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
 /* eslint-disable import/no-dynamic-require */
-
+const ClubIdNotDefinedError = require('./error/clubIdNotDefinedError');
+const ClubNotFoundError = require('./error/clubNotFoundError');
 const AbstractRepository = require('../../abstractRepository');
-
+const { fromDbToEntity } = require('../mapper/equipoMapper');
 /* eslint-disable eqeqeq */
 module.exports = class ClubRepository extends AbstractRepository {
-  constructor(fileSystem, dbPath) {
+  /**
+   *
+   * @param {import('better-sqlite3').Database} databaseAdapter
+   */
+  constructor(databaseAdapter) {
     super();
-    this.fileSystem = fileSystem;
-    this.dbPath = dbPath;
+    this.databaseAdapter = databaseAdapter;
   }
 
-  async save(equipo) {
-    const equipos = await this.getData();
-    equipos.push(equipo);
-    const jsonNewEquipo = JSON.stringify(equipos, null, 2);
-    this.saveData(jsonNewEquipo);
-  }
+  /**
+  *
+  * @param {import('../entidades/equipo')} equipo
+  */
+  save(equipo) {
+    let id;
+    const isUpdate = equipo.id;
 
-  async update(equipo) {
-    const equipos = await this.getData();
-    const equiposUpdate = equipos.map((dato) => {
-      if (dato.id === equipo.id) {
-        const result = Object.assign(dato, equipo);
-        return result;
+    if (isUpdate) {
+      id = equipo.id;
+      const statement = this.databaseAdapter.prepare(
+        `UPDATE equipos SET
+        ${equipo.crestUrl ? 'crest_url= ?,' : ''}
+        area_name = ?,
+        name = ?,
+        short_name = ?,
+        address = ?
+        WHERE id = ?
+        `);
+      const params = [
+
+        equipo.areaName,
+        equipo.name,
+        equipo.shortName,
+        equipo.address,
+        equipo.id,
+      ];
+      if (equipo.crestUrl) {
+        params.unshift(equipo.crestUrl);
       }
-      return dato;
-    });
-    const jsonNewEquipo = JSON.stringify(equiposUpdate, null, 2);
-    this.saveData(jsonNewEquipo);
-  }
-
-  async delete(equipoid) {
-    let equipos = await this.getData();
-    equipos = equipos.filter((equipoParam) => equipoParam.id != equipoid);
-    const jsonNewEquipo = JSON.stringify(equipos, null, 2);
-    this.saveData(jsonNewEquipo);
-  }
-
-  async getAll() {
-    return this.getData();
-  }
-
-  async getById(equipoid) {
-    const equipos = await this.getData();
-    return equipos.filter((equipoParam) => equipoParam.id == equipoid);
-  }
-
-  getData() {
-    const content = this.fileSystem.readFileSync(this.dbPath, { encoding: 'utf-8' });
-    let parsedContent;
-    try {
-      parsedContent = JSON.parse(content);
-    } catch (e) {
-      parsedContent = [];
+      statement.run(params);
+    } else {
+      const statement = this.databaseAdapter.prepare(`
+      INSERT INTO equipos(
+        area_name,
+        name,
+        short_name,
+        crest_url,
+        address
+      ) VALUES(?,?,?,?,?)
+    `);
+      const result = statement.run(
+        equipo.areaName,
+        equipo.name,
+        equipo.shortName,
+        equipo.crestUrl,
+        equipo.address,
+      );
+      id = result.lastInsertRowid;
     }
-    return parsedContent;
+    return this.getById(id);
   }
 
-  saveData(content) {
-    this.fileSystem.writeFileSync(this.dbPath, content, 'utf-8');
+  delete(equipo) {
+    if (!equipo || !equipo.id) {
+      throw new ClubIdNotDefinedError('El ID del equipo no esta definido');
+    }
+    this.databaseAdapter.prepare('DELETE FROM equipos WHERE id= ?').run(equipo.id);
+    return true;
+  }
+
+  getAll() {
+    const equipos = this.databaseAdapter
+      .prepare(
+        `SELECT 
+        id,
+        area_name,
+        name,
+        short_name,
+        crest_url,
+        address
+        FROM equipos`,
+      )
+      .all();
+
+    return equipos.map((equipoData) => fromDbToEntity(equipoData));
+  }
+
+  getById(id) {
+    const equipo = this.databaseAdapter
+      .prepare(`
+      SELECT 
+      id,
+      area_name,
+      name,
+      short_name,
+      crest_url,
+      address
+      FROM equipos WHERE id = ?`).get(id);
+    if (equipo === undefined) {
+      throw new ClubNotFoundError(`No se encontro el equipo con Id: ${id}`);
+    }
+    return fromDbToEntity(equipo);
   }
 };
