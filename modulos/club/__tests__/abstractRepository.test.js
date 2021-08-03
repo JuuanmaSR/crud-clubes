228b5354a841c989d19f6ec9da63a81f156147c5
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-08-03 00:37:42 -0300

    new-test-with-new-errors

--- modulos/club/__tests__/abstractRepository.test.js
+++ modulos/club/__tests__/abstractRepository.test.js
@@ -1,6 +1,8 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-undef */
 const AbstractRepository = require('../../abstractRepository');
 const AbstractErrorRepository = require('../../error/abstractErrorRepository');
+const MethodNotImplementedError = require('../repository/error/MethodNotImplementedError');
 
 test('No se puede instanciar un respositorio abstracto', () => {
   let repositoryInstance;
@@ -18,4 +20,14 @@ test('Se puede instanciar un repositorio concreto que herede del repositorio abs
   const repositoryInstance = new ConcreteRepository();
   expect(repositoryInstance).toBeInstanceOf(ConcreteRepository);
   expect(repositoryInstance).toBeInstanceOf(AbstractRepository);
+});
+
+test('Llamar a los metodos base sin implementacion concreta da error', () => {
+  const ConcreteRepository = class extends AbstractRepository {};
+  const repositoryInstance = new ConcreteRepository();
+
+  expect(() => repositoryInstance.save()).rejects.toThrowError(MethodNotImplementedError);
+  expect(() => repositoryInstance.delete()).rejects.toThrowError(MethodNotImplementedError);
+  expect(() => repositoryInstance.getById()).rejects.toThrowError(MethodNotImplementedError);
+  expect(() => repositoryInstance.getAll()).rejects.toThrowError(MethodNotImplementedError);
 });
