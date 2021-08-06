94753f0a73401c6cbb29e329cd5883ab9517e9d0
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-08-06 13:44:39 -0300

    modulo-club
    Ahora el modulo club tiene para ofrecer un modelo de equipo para interactuar con el repository

--- modulos/club/module.js
+++ modulos/club/module.js
@@ -1,9 +1,11 @@
 const ClubController = require('./controllers/clubController');
 const ClubService = require('./service/clubService');
 const ClubRepository = require('./repository/clubRepository');
+const EquipoModel = require('./model/equipoModel');
 
 module.exports = {
   ClubController,
   ClubService,
   ClubRepository,
+  EquipoModel,
 };
